0a29055a3d79bbe7410748db7e4048fc28bac5ff
brysah <brysahvidal@gmail.com>
2023-10-09 13:12:03 -0300

    FEAT: adiciona aviso do resultado

--- script.js
+++ script.js
@@ -1,33 +1,47 @@
-let cpf = "625.818.740-40";
-let cpfLimpo = cpf.replace(/\D+/g, '');
-let cpfArrayCompleto = Array.from(cpfLimpo);
-let cpfIncompleto = [...cpfArrayCompleto];
-cpfIncompleto.splice(9, 2);
-let acumulador = 0;
-let contador = 10;
-for (let numeros of cpfIncompleto) {
-    acumulador += contador * numeros;
-    contador--;
-}
-let primeiroDigito = 11 - (acumulador % 11);
-if (primeiroDigito > 9) primeiroDigito = 0;
-cpfIncompleto.push(primeiroDigito);
-acumulador = 0;
-contador = 11;
-for (let numeros of cpfIncompleto) {
-    acumulador += contador * numeros;
-    contador--;
-}
+const form = document.querySelector('form');
+const inputTxt = document.getElementById('campo-cpf');
+const result = document.querySelector('.message');
 
-let segundoDigito = 11 - (acumulador % 11);
-if (segundoDigito > 9) segundoDigito = 0;
-cpfIncompleto.push(segundoDigito);
+form.addEventListener('submit', (event) => {
+    event.preventDefault();
+    let cpf = inputTxt.value;
+    let cpfLimpo = cpf.replace(/\D+/g, '');
+    let cpfArrayCompleto = Array.from(cpfLimpo);
+    let cpfIncompleto = [...cpfArrayCompleto];
+    cpfIncompleto.splice(9, 2);
+    let acumulador = 0;
+    let contador = 10;
+    for (let numeros of cpfIncompleto) {
+        acumulador += contador * numeros;
+        contador--;
+    }
+    let primeiroDigito = 11 - (acumulador % 11);
+    if (primeiroDigito > 9) primeiroDigito = 0;
+    cpfIncompleto.push(primeiroDigito);
+    acumulador = 0;
+    contador = 11;
+    for (let numeros of cpfIncompleto) {
+        acumulador += contador * numeros;
+        contador--;
+    }
 
-let cpfResultado = cpfIncompleto.map(Number);
-cpfResultado = Number(cpfResultado.join(''));
-console.log(cpfResultado);
-cpfLimpo = parseInt(cpfLimpo);
-console.log(cpfLimpo);
-if(cpfResultado === cpfLimpo){
-    console.log("CPF VÁLIDO");
-}else console.log("cpf invalido");
+    let segundoDigito = 11 - (acumulador % 11);
+    if (segundoDigito > 9) segundoDigito = 0;
+    cpfIncompleto.push(segundoDigito);
+
+    let cpfResultado = cpfIncompleto.map(Number);
+    cpfResultado = Number(cpfResultado.join(''));
+    console.log(cpfResultado);
+    cpfLimpo = parseInt(cpfLimpo);
+    console.log(cpfLimpo);
+    if (cpfResultado === cpfLimpo) {
+        result.style.setProperty('opacity',1);
+        result.style.setProperty('color','lightgreen');
+        result.textContent = "CPF VÁLIDO";
+    } else{
+        result.style.setProperty('opacity',1);
+        result.style.setProperty('color','red');
+        result.textContent = "CPF INVÁLIDO";
+    }
+});
+
