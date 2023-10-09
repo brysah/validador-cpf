6a228910d7c11bc1b3f7a2f5fc64bed596946b8f
brysah <brysahvidal@gmail.com>
2023-10-09 11:39:15 -0300

    FEAT: adiciona calculo do segundo digito

--- script.js
+++ script.js
@@ -3,7 +3,8 @@ let cpfLimpo = cpf.replace(/\D+/g, '');
 let cpfArrayCompleto = Array.from(cpfLimpo);
 let cpfIncompleto = [...cpfArrayCompleto];
 cpfIncompleto.splice(9,2);
-// console.log(cpfIncompleto);
+//  console.log(cpfIncompleto);
+//  console.log(cpfArrayCompleto);
 let acumulador = 0;
 let contador = 10;
 for( let numeros of cpfIncompleto){
@@ -13,4 +14,18 @@ for( let numeros of cpfIncompleto){
 console.log(acumulador);
 let primeiroDigito = 11 - (acumulador % 11);
 if(primeiroDigito > 9) primeiroDigito = 0;
-console.log(primeiroDigito);
+cpfIncompleto.push(primeiroDigito);
+// console.log(cpfIncompleto);
+  acumulador = 0;
+  contador = 11;
+for( let numeros of cpfIncompleto){
+    acumulador += contador * numeros;
+    contador--;
+}
+
+console.log(acumulador);
+
+let segundoDigito = 11 - (acumulador % 11);
+if(segundoDigito > 9) segundoDigito = 0;
+console.log(segundoDigito);
+cpfIncompleto.push(segundoDigito);
