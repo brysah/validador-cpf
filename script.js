71e0f213cd42f3de8237da7c1350e3487c318f6f
brysah <brysahvidal@gmail.com>
2023-10-09 11:22:57 -0300

    FEAT: realiza a primeira parte do calculo

--- script.js
+++ script.js
@@ -4,3 +4,10 @@ let cpfArrayCompleto = Array.from(cpfLimpo);
 let cpfIncompleto = [...cpfArrayCompleto];
 cpfIncompleto.splice(9,2);
 // console.log(cpfIncompleto);
+let acumulador = 0;
+let contador = 10;
+for( let numeros of cpfIncompleto){
+    acumulador += contador * numeros;
+    contador--;
+}
+// console.log(acumulador);
