44b4084317c7ac3c57d64f9a5d7101dc13d0b256
brysah <brysahvidal@gmail.com>
2023-10-09 11:46:32 -0300

    FEAT: Adiciona resultado final

--- script.js
+++ script.js
@@ -2,34 +2,32 @@ let cpf = "705.484.450-52";
 let cpfLimpo = cpf.replace(/\D+/g, '');
 let cpfArrayCompleto = Array.from(cpfLimpo);
 let cpfIncompleto = [...cpfArrayCompleto];
-cpfIncompleto.splice(9,2);
-//  console.log(cpfIncompleto);
-//  console.log(cpfArrayCompleto);
+cpfIncompleto.splice(9, 2);
 let acumulador = 0;
 let contador = 10;
-for( let numeros of cpfIncompleto){
+for (let numeros of cpfIncompleto) {
     acumulador += contador * numeros;
     contador--;
 }
-console.log(acumulador);
 let primeiroDigito = 11 - (acumulador % 11);
-if(primeiroDigito > 9) primeiroDigito = 0;
+if (primeiroDigito > 9) primeiroDigito = 0;
 cpfIncompleto.push(primeiroDigito);
-// console.log(cpfIncompleto);
-  acumulador = 0;
-  contador = 11;
-for( let numeros of cpfIncompleto){
+acumulador = 0;
+contador = 11;
+for (let numeros of cpfIncompleto) {
     acumulador += contador * numeros;
     contador--;
 }
-
-console.log(acumulador);
 
 let segundoDigito = 11 - (acumulador % 11);
-if(segundoDigito > 9) segundoDigito = 0;
-console.log(segundoDigito);
+if (segundoDigito > 9) segundoDigito = 0;
 cpfIncompleto.push(segundoDigito);
 
-let arrayNumero = cpfIncompleto.map(Number);
-arrayNumero = Number(arrayNumero.join(''));
-console.log(arrayNumero);
+let cpfResultado = cpfIncompleto.map(Number);
+cpfResultado = Number(cpfResultado.join(''));
+console.log(cpfResultado);
+cpfLimpo = parseInt(cpfLimpo);
+console.log(cpfLimpo);
+if(cpfResultado === cpfLimpo){
+    console.log("CPF VÁLIDO");
+}else console.log("cpf invalido");
